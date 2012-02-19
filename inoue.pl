78c069fb935347dc93fdc64c1b2909cde9729110
Kanoe Hiromi <kyou.wk@gmail.com>
2012-02-19 12:07:06 +0800

    Changed

--- inoue.pl
+++ inoue.pl
@@ -1,25 +1,28 @@
 #!/usr/bin/perl
 
+#Usage
+#perl inoue.pl /path/to/top/directory/
 #Renames file.jpg to md5(file).jpg
 #matches *.jpg and *.jpg.* (wget repetitions)
 #By Hiromi
 
+use File::Find;
 use Digest::MD5;
 $md5 = new Digest::MD5;
 
-opendir(DIR, "./");
-my @files = readdir(DIR);
+@directories = shift;
 
+finddepth(\&hash, @directories);
 
-foreach my $files (@files) {
-        if ($files =~ /$\.jpg\.(.)*/ || $files =~ /$\.jpg/){
-                open(FILE, $files) or die "Can't open '$files': $!";
+sub hash{
+        if ($File::Find::name =~ /$\.jpg\.(.)*/ || $File::Find::name =~ /$\.jpg/){
+                open(FILE, $File::Find::name) or die "Can't open '$File::Find::name': $!";
                 seek(FILE, 0, 0);
                 $md5->reset;
                 $md5->addfile(FILE);
                 $d = $md5->hexdigest;
-                printf "$d $files\n";
+                printf "$d $File::Find::name\n";
                 $f2 = $d . ".jpg";
-                rename($files, $f2) or die "Herped";
-                }
-        }
+                rename($File::Find::name, $f2) or die "Herped";
+         }
+}
