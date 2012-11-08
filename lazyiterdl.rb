4bdd06d25f82da3c1d3df9e76b1b176ecd43bb89
Hiromi-nee <kyou.wk@gmail.com>
2012-11-08 09:44:19 +0800

    lazy script

---
+++ lazyiterdl.rb
@@ -0,0 +1,9 @@
+#lazy script
+payload = "wget http://someurl/"
+tailpayload = ".extension"
+(1..99).each do |n|
+  final = payload + n.to_s + tailpayload
+  p final
+  system(final)
+  sleep(0.1)
+end
