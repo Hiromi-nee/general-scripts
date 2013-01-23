caa90faf7efdfaf2e015b084178a12331a16f503
Kanoe Hiromi <kyou.wk@gmail.com>
2013-01-23 18:25:02 +0800

    inoue.py

---
+++ inoue.py
@@ -0,0 +1,32 @@
+import os
+import sys
+import hashlib
+import re
+directory = ""
+
+jpg_re = re.compile("(.*)(\.jpg\.\d*|\.jpg|\.jpeg|\.jpeg\.\d*)$")
+
+def sha1sum(filename):
+    sha1 = hashlib.sha1()
+    with open(filename, 'rb') as f:
+        for chunk in iter(lambda: f.read(128*sha1.block_size), b''):
+            sha1.update(chunk)
+    return sha1.hexdigest()
+
+def main():
+    directory = sys.argv[1]
+    
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if jpg_re.match(file):
+                sha1hash = sha1sum(root+"/"+file)
+                new_name = sha1hash + ".jpg"
+                print root+"/"+file +" :: " + sha1hash
+                os.rename(root+"/"+file, root+"/"+new_name)
+
+if __name__ == "__main__":  
+    if len(sys.argv) == 2:
+        main()
+    else:
+        print "Usage: python "+sys.argv[0]+" <directory>"
+
